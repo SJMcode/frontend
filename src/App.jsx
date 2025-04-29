cde8375d97cad4f073c8e2aa5680d1fd51ee6f65
Safir <safir.jameel@gmail.com>
2025-04-30 00:31:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ const App = () => {
   const getData = async () => {
     try {
       const [educationResponse, workResponse, portfolioResponse] = await Promise.all([
-        fetch("api/education/"),
-        fetch("api/work/"),
-        fetch("api/portfolio/")
+        fetch("/education/"),
+        fetch("/work/"),
+        fetch("/portfolio/")
       ]);
       
       const [educationData, workData, portfolioData] = await Promise.all([
